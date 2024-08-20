9f7d1b3e20eea342bb55410fd5dc779d3756bc45
AdmiN <AdmiN@ITWS0038>
2024-08-20 11:01:51 +0530

    implements signup and login pages

--- src/Login.java
+++ src/Login.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class Login {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        String uname="anitha@gmail.com";
+        String pwd="12345";
+        System.out.print("Welcome to Github");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter username");
+        String uname1=sc.next();
+        System.out.print("Enter password");
+        String pwd1=sc.next();
+        if(uname.equals(uname1)&&pwd.equals(pwd1)) {
+        	System.out.print("Login Successfull");
+        }
+        else {
+        	System.out.print("Login Failed");
+        }
 	}
 
 }
--- src/Signup.java
+++ src/Signup.java
@@ -1,9 +1,16 @@
-
+import java.util.*;
 public class Signup {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        System.out.print("Welcome to Github");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter username");
+        String uname=sc.next();
+        System.out.print("Enter password");
+        String pwd=sc.next();
+        System.out.print("Your username"+uname);
+        System.out.print("Your password"+pwd);
 	}
 
 }
